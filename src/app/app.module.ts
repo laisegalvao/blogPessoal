dc00449fa381cbdf90ec97f176bba6922aaa7dac
laise <lg.souza15@gmail.com>
2021-01-18 18:10:54 -0300

    Componentes do Blog criados(Menu,rodapé, Login e Cadastro)

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RodapeComponent } from './rodape/rodape.component';
+import { MenuComponent } from './menu/menu.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RodapeComponent,
+    MenuComponent,
+    EntrarComponent,
+    CadastrarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
